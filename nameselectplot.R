e735834059ce5b07292de0698e6287469702edfe
Bill Seliger <bill.seliger@gmail.com>
2015-01-01 15:31:35 -0600

    Adding the nameselectplot script

---
+++ nameselectplot.R
@@ -0,0 +1,35 @@
+## based on the selected name prints the proportion of children given that name each year by sex 
+## and prints a plot of the same data
+
+nameselectplot <- function(selectname = "Victoria") {  ##Default selectname is Victoria
+  
+  require(reshape2)  ## reshape2 required for the melt function
+  require(ggplot2)   ## ggplot2 required for the ggplot function
+  
+  baby<-read.csv("baby-names.csv")  ## read in the names file
+ 
+ boynames <- subset (baby, sex == "boy")
+ girlnames <- subset (baby, sex == "girl")
+ 
+ boy.results <- subset (boynames, boynames$name == selectname)
+ girl.results <- subset (girlnames, girlnames$name == selectname)
+ 
+ results <- merge(boy.results, girl.results, by = "year", all = TRUE)
+ 
+ results$sex.x <- NULL
+ results$sex.y <- NULL
+ results$name.x <- NULL
+ results$name.y <- NULL
+ 
+ colnames(results) <- c("year", "boy proportion", "girl proportion")
+ 
+ print(selectname)
+ print(results)
+ 
+ results_long <- melt(results, id="year")  # convert to long format
+ 
+ ggplot(data=results_long,
+        aes(x=year, y=value, colour=variable)) +
+   geom_line() + ggtitle(selectname) + theme(plot.title = element_text(size=32, face="bold", vjust=1))
+}
+
